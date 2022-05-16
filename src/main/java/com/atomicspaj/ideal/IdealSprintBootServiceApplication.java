4c33774aa84f760ebcdd3f05a7b063c189c5b057
Ajith Kumar Jain <atomicspaj@gmail.com>
2022-05-16 23:43:40 +0530

    Initial commit

---
+++ src/main/java/com/atomicspaj/ideal/IdealSprintBootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atomicspaj.ideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealSprintBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealSprintBootServiceApplication.class, args);
+	}
+
+}
